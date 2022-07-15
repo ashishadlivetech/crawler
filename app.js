85993881e1b6c71211631f25946ed6e4b4349acf
ashish <sggofficials@grabdeal.co.in>
2022-07-15 10:33:05 +0530

    aswe

--- app.js
+++ app.js
@@ -1,78 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const Wappalyzer = require('wappalyzer');
+const rp = require('request-promise');
 const axios = require("axios");
 const cheerio = require("cheerio");
 var url = '';
-var crash= false;
-var results;
 require('dotenv').config();
 const port = process.env.PORT;
 
-const options = {
-    debug: false,
-    delay: 500,
-    headers: {},
-    maxDepth: 3,
-    maxUrls: 10,
-    maxWait: 80000, 
-    recursive: true,
-    probe: true,
-    proxy: false,
-    userAgent: 'Wappalyzer',
-    htmlMaxCols: 2000,
-    htmlMaxRows: 2000,
-    noScripts: false,
-    noRedirect: false,
-  };
-  
-  const wappalyzer = new Wappalyzer(options)
-  
 
-// server css as static
 app.use(express.static(__dirname));
 
-// get our app to use body parser 
-app.use(bodyParser.urlencoded({ extended: true }))
 
-const reqFilter = (req,res,next)=>{ 
-    if((req.method == 'POST') && (req.body.url)){
-       url = req.body.url;
-       (async function() {
-        try {
-          await wappalyzer.init()
-      
-          // Optionally set additional request headers
-          const headers = {}
-          
-          const site = await wappalyzer.open(url, headers)
-      
-          // Optionally capture and output errors
-          // site.on('error', console.error)
-      
-           results = await site.analyze()
-         
-        } catch (error) {
-            console.log(error);
-        }
-      await wappalyzer.destroy();
-       if((results) && (results.technologies)){
-              for(p=0; p < results.technologies.length; p++){
-                if(results.technologies[p].slug == 'woocommerce'){ 
-                  crash = true;
-                  res.send('Provided link is verify that it is related with Woocommrce Now Please<a href="/details"> Click here</a> for get product detail ');
-                }
-            }
-            if(!crash){
-              res.send('Provided link not related to woocommerce <a href="/">Please use other URL</a>');
-            }
-          }
-      })();
-    }else{ 
-        next();
-    } 
-}
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const fetchShelve = async () => {
     try { 
@@ -123,20 +63,40 @@ const fetchShelve = async () => {
         throw error;
     }
 };
-app.use(reqFilter);
+
 app.listen(port, () => {
   console.log("Application started and Listening on port"+ port);
 });
-
-
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", (req, res) => {
-  
+  url = req.body.url;
+  rp(url)
+  .then(function(html){ 
+    if(html.includes('wp-admin')){
+      res.send('Provided Url related with Wordpress Please  <a href="woocommerce">click here</a> for check Woocommerce');
+    }
+  })
+  .catch(function(err){
+    res.send('Your Url not related with Wordpress Please  <a href="/">click here</a>');
+  });
 });
+
+app.get("/woocommerce", (req, res) => {
+  rp(url)
+  .then(function(html){ 
+    if(html.includes('woocommerce')){
+      res.send('Provided Url related with Woocommerce Please check here for get all data <a href="details">first here</a>');
+    }
+  })
+  .catch(function(err){
+    res.send('Your Url not related with woocommerce Please click here <a href="/">first here</a>');
+  });
+});
+
 app.get("/details", (req, res) => {
   if(!url){
     res.send('Please provide url <a href="/">first here</a>');
